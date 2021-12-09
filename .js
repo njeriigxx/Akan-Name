94ebabbd3d92b7e719d2e587a84526b9dade4290
njeriigxx <njeriigathuku@gmail.com>
2021-12-09 23:10:55 +0300

    Added variables dates, femaleNames and maleNames

---
+++ .js
@@ -0,0 +1,3 @@
+var femaleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw" ,"Kofi", "Kwame" ]
+var maleNames = ["Akosua", "Adwoa" ,"Abena" ,"Akua" ,"Yaa" ,"Afua" ,"Ama"]
+var weekDays = ["Sundays", "Monday" ,"Tuesday" ,"Wednesday" ,"Thursday" ,"Friday" ,"Saturday"]
